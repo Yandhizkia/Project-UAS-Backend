adafe2e10bd9c8d1f6f2b460f0a85516b09d81aa
Richard V.H <godof7sea@gmail.com>
2025-06-18 12:45:59 +0700

    Update output.blade.php

--- resources/views/tiket/output.blade.php
+++ resources/views/tiket/output.blade.php
@@ -3,3 +3,7 @@
 <p>Tipe Tiket: {{ $ticket->tipe }}</p>
 <p>Jumlah Tiket dibeli: {{ $jumlah }}</p>
 <p>Total Harga: Rp. {{ number_format($total, 0, ',', '.') }}</p>
+
+<a href="{{ url('/') }}">
+    <button type="button">Exit</button>
+</a>
